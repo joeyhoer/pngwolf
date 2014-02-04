fa3b02116d27ffc81b57485547f2acf459587fb5
Joey Hoer <me@joeyhoer.com>
2014-02-03 20:06:12 -0500

    Adding .gitignore, updating build script, updating patch, reorganizing folder structure

--- build.sh
+++ build.sh
@@ -1,56 +1,71 @@
 #!/usr/bin/env bash
 
+##
+# Ensure proper working directory
+##
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Download dependancies
+
+##
+# Make/save directories
+##
+TMP="tmp"
+mkdir -p $TMP
+
 SRC="src"
 mkdir -p $SRC
 
+PAT="patches"
+
+##
+# Download dependancies
+##
 echo "Downloading GALib"
-curl -q#L "http://lancet.mit.edu/ga/dist/galib247.tgz" > "$SRC/galib.tgz"
+curl -q#L "http://lancet.mit.edu/ga/dist/galib247.tgz" > "$TMP/galib.tgz"
 
 echo "Downloading 7-Zip"
 # This could break as it's using the "latest" version
-curl -q#L "http://sourceforge.net/projects/p7zip/files/latest/download" > "$SRC/p7zip.tar.bz2"
+curl -q#L "http://sourceforge.net/projects/p7zip/files/latest/download" > "$TMP/p7zip.tar.bz2"
 # Alternatively, specify a specific version
-# curl -#L "http://sourceforge.net/projects/sevenzip/files/7-Zip/9.20/7z920.tar.bz2/download" > "$SRC/p7zip.tar.bz2"
+# `projects/sevenzip/files/7-Zip/9.20/7z920.tar.bz2/download`
 
 echo "Downloading zlib"
-curl -q#L "http://zlib.net/zlib-1.2.8.tar.xz" > "$SRC/zlib.tar.xz"
-
-echo "Cloning Zopfli"
-git clone https://code.google.com/p/zopfli/
-rm -Rf "$SRC/zopfli/.git"
+curl -q#L "http://zlib.net/zlib-1.2.8.tar.gz" > "$TMP/zlib.tar.gz"
 
 
+##
 # Extract source files
+##
 echo "Extracting GALib"
 mkdir -p "galib"
-tar xf "$SRC/galib.tgz"     --strip-components 1  -C "galib"
+tar -xzf "$TMP/galib.tgz"     --strip-components 1  -C "$SRC/galib"
 
 echo "Extracting 7-Zip"
 mkdir -p "7zip"
-tar xf "$SRC/p7zip.tar.bz2" --strip-components 1  -C "7zip"
+tar -xjf "$TMP/p7zip.tar.bz2" --strip-components 1  -C "$SRC/7zip"
 
 echo "Extracting zlib"
 mkdir -p "zlib"
-tar xf "$SRC/zlib.tar.xz"   --strip-components 1  -C "zlib"
+tar -xzf "$TMP/zlib.tar.gz"   --strip-components 1  -C "$SRC/zlib"
 
 
+##
 # Apply patches
-PAT="patches"
-
+##
 echo "Patching GALib"
 patch -fp 0 < "$PAT/galib247.patch"
 
 echo "Patching 7-Zip"
 patch -fp 0 < "$PAT/sevenzip9201.patch"
+# Patch using bash
 # Remove references to Visual Studio precompiled headers
-grep -lR '#include "StdAfx.h"' ./7zip/ | xargs sed -i "" -E 's/#include "StdAfx.h"//g'
+grep -lR '#include "StdAfx.h"' "$SRC/7zip" | xargs sed -i "" -E 's/#include "StdAfx.h"//g'
 
 
-# Make
+##
+# Make binary
+##
 echo "Making binary"
 cmake -H. -Bbuild #CMakeLists.txt
 make -C build/
